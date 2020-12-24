d26f851d126860cf99c1af48637496acf43399a8
unknown <dudtjr913@naver.com>
2020-12-24 17:26:41 +0900

    다각형 넓이 구하기

--- index.js
+++ index.js
@@ -1,29 +1,56 @@
-const createCircle = (radius) => {
-  if (checkParameter(radius)) {
-    return;
+const getArea = (polygon, ...args) => {
+  switch (polygon) {
+    case 'circle':
+      getCircle(...args);
+      break;
+    case 'rect':
+      getRect(...args);
+      break;
+    case 'trapezoid':
+      getTrapezoid(...args);
+      break;
+    case 'cylinder':
+      getCylinder(...args);
+      break;
+    default:
+      break;
   }
-
-  return radius ** 2 * Math.PI;
 };
 
-const createSquare = (left, top) => {
-  if (checkParameter(left, top)) {
-    return;
+const getCircle = (() => {
+  let sum = 0;
+  return (radius, count) => {
+    if (!checkParameter(radius)) {
+      return;
+    }
+    if (count < 1) {
+      return;
+    }
+
+    sum += radius ** 2 * Math.PI;
+    getCircle(radius + 1, count - 1);
+    return sum;
+  };
+})();
+
+const getRect = (left, top) => {
+  if (!checkParameter(left, top)) {
+    return false;
   }
 
   return left * top;
 };
 
-const createTrapezoid = (top, bottom, height) => {
-  if (checkParameter(top, bottom, height)) {
+const getTrapezoid = (top, bottom, height) => {
+  if (!checkParameter(top, bottom, height)) {
     return;
   }
 
   return ((top + bottom) * height) / 2;
 };
 
-const createCylinder = (radius, height) => {
-  if (checkParameter(radius, height)) {
+const getCylinder = (radius, height) => {
+  if (!checkParameter(radius, height)) {
     return;
   }
 
@@ -34,10 +61,14 @@ const checkParameter = (...args) => {
   const argValue = args.every((arg) => arg);
   const allArgsTypeNumber = args.every((arg) => typeof arg === 'number');
   if (!argValue) {
-    return error('인자의 개수가 부족합니다.');
+    return alert('인자의 개수가 부족합니다.');
   }
 
   if (!allArgsTypeNumber) {
-    return error('숫자를 입력해주세요.');
+    return alert('숫자를 입력해주세요.');
   }
+
+  return true;
 };
+
+console.log(getCircle(3, 3));
