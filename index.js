d159122de547e7d8fe7f0b0624ba58163a1d945e
unknown <dudtjr913@naver.com>
2020-12-24 11:22:03 +0900

    숫자가 아니면 에러 출력

--- index.js
+++ index.js
@@ -1,15 +1,40 @@
 const createCircle = (radius) => {
+  if (checkParameter(radius)) {
+    return;
+  }
+
   return radius ** 2 * Math.PI;
 };
 
 const createSquare = (left, top) => {
+  if (checkParameter(left, top)) {
+    return;
+  }
+
   return left * top;
 };
 
 const createTrapezoid = (top, bottom, height) => {
+  if (checkParameter(top, bottom, height)) {
+    return;
+  }
+
   return ((top + bottom) * height) / 2;
 };
 
 const createCylinder = (radius, height) => {
+  if (checkParameter(radius, height)) {
+    return;
+  }
+
   return 2 * Math.PI * radius ** 2 + 2 * Math.PI * radius * height;
 };
+
+const checkParameter = (...args) => {
+  const allArgsTypeNumber = args.every((arg) => typeof arg === 'number');
+  if (!allArgsTypeNumber) {
+    return error('숫자를 입력해주세요.');
+  }
+};
+
+createTrapezoid(1, 2, 3);
